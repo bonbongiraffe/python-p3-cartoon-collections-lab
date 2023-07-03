8edc9071d0a7f4622f4675d37bc9a953011f42d7
bonbongiraffe <francescowai@yahoo.com>
2023-07-03 17:53:27 -0400

    first attempt

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,23 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    counter = 1
+    for dwarve in dwarves:
+        print(f'{counter}. {dwarve}')
+        counter += 1
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(calls):
+    cp_calls = []
+    for call in calls:
+        cp_calls.append(f'{(call[0]).upper()}{call[1:]}!')
+    return cp_calls
 
-def long_planeteer_calls():
-    pass
 
-def find_the_cheese():
-    pass
+def long_planeteer_calls(calls):
+    for call in calls:
+        if len(call) > 4:
+            return True
+    return False
+
+def find_the_cheese(items):
+    for item in items:
+        if item == "cheddar":
+            return item
